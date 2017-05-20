de4a8c2f58eb2f4878fe8bc4f4f40899119b2586
Max <maxburda@gmail.com>
2017-05-20 21:45:24 +0300

    planned commit

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
-const server = app.listen(8080, () =>{
-  console.log(`server is up and running on port 8080`);
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const server = app.listen(8080, () => {
+    console.log(`Server is up and running on port 8080`);
 });
